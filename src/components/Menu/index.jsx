a5700d8d49ab8dbe1006f8ade2341676b62f05cc
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-10-21 15:21:10 +0300

    Komponenta Drink

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,10 +1,8 @@
+import {Drink} from '../Drink';
 import './menu.css';
 
 export const Menu = () => {
     return (
-
-
-
 <section className="menu" id="menu" >
 <div className="container" >
   <h2>Naše nabídka</h2>
@@ -12,75 +10,25 @@ export const Menu = () => {
     Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
   </p>
   <div className="drinks-list">
-    <div className="drink">
-      <div className="drink__product">
-        <div className="drink__cup">
-          <img src="/cups/espresso.png" />
-        </div>
-        <div className="drink__info">
-          <h3>Espresso</h3>
-          <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-            <div className="layer__label">espresso</div>
-          </div>
-        </div>
-      </div>
-      <div className="drink__controls">
-        <button className="order-btn">
-          Objednat
-        </button>
-      </div>
-    </div>
-
-    <div className="drink">
-      <div className="drink__product">
-        <div className="drink__cup">
-          <img src="/cups/doppio.png" />
-        </div>
-        <div className="drink__info">
-          <h3>Doppio</h3>
-          <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-            <div className="layer__label">espresso</div>
-          </div>
-        </div>
-      </div>
-      <div className="drink__controls">
-        <button className="order-btn">
-          Objednat
-        </button>
-      </div>
-    </div>
-
-    <div className="drink">
-      <div className="drink__product">
-        <div className="drink__cup">
-          <img src="/cups/lungo.png" />
-        </div>
-        <div className="drink__info">
-          <h3>Lungo</h3>
-          <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#b0dee1' }} />
-            <div className="layer__label">voda</div>
-          </div>
-          <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-            <div className="layer__label">espresso</div>
-          </div>
-        </div>
-      </div>
-      <div className="drink__controls">
-        <button className="order-btn">
-          Objednat
-        </button>
-      </div>
-    </div>
+    
+  <Drink
+  id={0}
+  name="Espresso"
+  ordered={false}
+  image="/cups/espresso.png"
+  layers={[
+    {
+      color: '#613916',
+      label: 'espresso',
+    },
+  ]}
+  />
   </div>
 
-  <div className="order-detail">
-    <a href="/objednavka">Detail objednávky</a>
-  </div>
+  <div className='order-detail'>
+<a href='/objednavka'>Detail objednavky</a>
+</div>
 </div>
 </section>
-    )
-}
+    );
+};
